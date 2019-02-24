5e1b44eadd70f37da6f84f6e740aeb3b5febd0e1
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2019-02-24 09:11:17 +0000

    search route works

---
+++ app/forms.py
@@ -0,0 +1,6 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+class SearchForm(FlaskForm):
+    searchterms = StringField('Search terms', validators=[DataRequired()])
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,17 @@
+from flask import render_template, jsonify
 from app import app
+from app.forms import SearchForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+
+    form = SearchForm()
+    return  render_template('index.html', title='Home', form=form)
+
+@app.route('/search/', methods=['post'])
+def search():
+    form = SearchForm()
+    if form.validate_on_submit():
+        return jsonify(data={'message': 'Search terms: {}'.format(form.searchterms.data)})
+    return jsonify(data=form.errors)
