eb4e8ce72f3beaab22561d85c287970728777019
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2019-02-25 13:12:37 +0000

    database builds from meta

--- app/models.py
+++ app/models.py
@@ -2,24 +2,30 @@ from app import db
 
 class Database(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True, unique=True)
-    description = db.Column(db.String(120), index=True, unique=True)
+    db_name = db.Column(db.String(64), index=True, unique=True)
+    db_desc = db.Column(db.String(120), index=True, unique=True)
+
+    tables = db.relationship('Table', backref='database', lazy='dynamic')
 
     def __repr__(self):
         return '<Name {}>'.format(self.name)
 
 class Table(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True, unique=True)
-    description = db.Column(db.String(120), index=True, unique=True)
+    tbl_name = db.Column(db.String(64), index=True, unique=True)
+    tbl_desc = db.Column(db.String(120), index=True, unique=True)
+    database_id = db.Column(db.Integer, db.ForeignKey('database.id'))
+
+    fields = db.relationship('Column', backref='table', lazy='dynamic')
 
     def __repr__(self):
-        return '<Name {}>'.format(self.name)
+        return '<Table name {}>'.format(self.tbl_name)
 
-class Field(db.Model):
+class Column(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True, unique=True)
-    description = db.Column(db.String(120), index=True, unique=True)
+    clm_name = db.Column(db.String(64), index=True)
+    clm_desc = db.Column(db.String(120), index=True)
+    table_id = db.Column(db.Integer, db.ForeignKey('table.id'))
 
     def __repr__(self):
-        return '<Name {}>'.format(self.name)
+        return '<Column name {}>'.format(self.name)
--- populate_db/populate_db.py
+++ populate_db/populate_db.py
@@ -1,19 +1,54 @@
 import sys
-import os.path
+import os
 sys.path.append(
     os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 
-from app.models import Database
+from app.models import Database, Table, Column
 from app import db
+import json
+
 
 if __name__ == "__main__":
+
 
     db.drop_all() #TODO probably make this optional!!
     db.create_all()
 
-    d = Database()
-    d.name = "hello3"
-    d.description = "first_db"
+    subfolders = [f.path for f in os.scandir("populate_db/metadata_folders/") if f.is_dir() ]
+    for subfolder in subfolders:
+        with open(os.path.join(subfolder, "database.json")) as f:
+            dbjson = json.load(f)
 
-    db.session.add(d)
-    db.session.commit()
+        d = Database()
+        d.db_name = dbjson["name"]
+        d.db_desc = dbjson["description"]
+
+        db.session.add(d)
+        db.session.commit()
+
+        files = os.listdir(subfolder)
+        files = [f for f in files if f != "database.json"]
+        table_files = [f for f in files if f.endswith(".json")]
+        for table_file in table_files:
+            with open(os.path.join(subfolder, table_file)) as f:
+                tbljson = json.load(f)
+
+            t = Table()
+            t.tbl_name = tbljson["name"]
+            t.tbl_desc = tbljson["description"]
+            t.database = d
+            db.session.add(t)
+            db.session.commit()
+
+            for column in tbljson["columns"]:
+                c = Column()
+                c.clm_desc = column["description"]
+                c.clm_name = column["name"]
+
+                c.table = t
+
+                db.session.add(c)
+                db.session.commit()
+
+
+
